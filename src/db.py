fa20caca61843e3254daa8cad1b00d14fb2066fc
yehia67 <yehiatarek67@gmail.com>
2019-08-07 15:42:45 +0200

    databe tables issues

--- src/db.py
+++ src/db.py
@@ -1,27 +1,35 @@
 import sqlite3
-def insert(value):
-
-    conn.execute("INSERT INTO ISSUE (MSG) \
-      VALUES (?)", (value,));
-    print("Records created successfully")  
-
-def show():
-    cursor = conn.execute("SELECT MSG from ISSUE")
-    for row in cursor:
-        print("MSG = "+row[0])
-        
-conn = sqlite3.connect('issue.db')
-
-print("Opened database successfully")
-
-""" conn.execute('''CREATE TABLE ISSUE
+def createIssue(issueName):
+    conn.execute('''CREATE TABLE {tableName}
          (
          MSG           char(200)    NOT NULL
-         );''') """ 
-insert("el7")
-insert("isa")
-insert("yarab yarab")
-show()
-print("Table created successfully")
+         );'''.\
+        format(tableName=issueName))
+    print(" Table created successfully")  
+
+def insert(issueName,value):
+    conn.execute("INSERT INTO {tableName} (MSG) \
+      VALUES (?)".\
+        format(tableName=issueName),(value,))
+    print("Records created successfully")  
+
+def show(issueName):
+    cursor = conn.execute("SELECT MSG from  {tableName}".\
+        format(tableName=issueName))
+    for row in cursor:
+        print("MSG = "+row[0])
+
+     
+conn = sqlite3.connect('issue.db')
+print("Opened database successfully")
+
+createIssue("el77")
+insert("el77","isa")
+insert("el77","yarab yarab") 
+insert("el77","yarab el7") 
+insert("el77","el7 el7 el7") 
+
+show("el77")
+
 
 conn.close()       
