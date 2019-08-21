481a68be1915e133ceb2dba4159ef43f624e5b00
Mostafa Hazem <mostafa17_2@live.com>
2019-08-21 13:37:03 +0200

    added a loder in repo page until commits are loaded

--- src/app.js
+++ src/app.js
@@ -457,11 +457,18 @@ App = {
         })
     },
     showCommits: async(boolValue) => {
-        if (boolValue) { //Show Initial Commits
+        //Hide Loader
+        document.getElementById('loader').style.display = 'none'
+
+        //Show Initial Commits
+        if (boolValue) {
             console.log(document.getElementById('commitsDiv'))
             document.getElementById('commitsDiv').style.display = 'block'
             document.getElementById('noCommitBtn').style.display = 'none'
-        } else { //No Initial Commits
+        }
+
+        //No Initial Commits
+        else {
             document.getElementById('commitsDiv').style.display = 'none'
             document.getElementById('noCommitBtn').style.display = 'block'
         }
